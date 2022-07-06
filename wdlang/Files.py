e250948fa7904a224720262b93da1e113e4d5fa9
NoachDev <noachdev@jitjat.org>
2022-07-06 15:03:54 +0000

    Src -> wdlang | issues fixed

---
+++ wdlang/Files.py
@@ -0,0 +1,92 @@
+from .Parser import WdReader 
+from pathlib import Path
+
+from importlib.machinery import SourceFileLoader
+
+class Files_structure(dict):
+  def __init__(self, Src_folder : str, Functions : None | str = None):
+    self.Src_folder   : Path        = Path(Src_folder).absolute()
+    self.Functions    : None | str  = Functions
+    self.varspresets  : dict        = {"wd_vars" : {}, "presets" : {}}
+
+    if not self.Functions:
+      self.Functions = self.Src_folder.parent.joinpath("Functions")
+    else:
+      self.Functions = Path(self.Functions)
+
+    self.update(self.Get_files(self.Src_folder))
+
+  def Get_files(self, src : Path) -> dict :
+    ret : dict = {}
+    pathl : list = list(src.iterdir())
+    pathl.sort()
+
+    for i in pathl:
+      if i.is_dir():
+        self.Get_files(i)
+      else:
+        name      = self.Transform_to_tkname(str(i))
+
+        script    =  self.Functions.joinpath(i.parent.relative_to(self.Src_folder)).joinpath(i.name.replace(".wd", "") + ".py")
+
+        if script.exists():
+          script  = SourceFileLoader("func", str(script)).load_module().Main
+        else:
+          script  = None
+
+        self.varspresets["wd_vars"]["__master__"] = name
+        wdlang    = WdReader(i, **self.varspresets)
+
+        ret.update({self.Chek_name(name, k) : {**v, "script" : script, "__master__" : name} for k, v in wdlang.widgets.items()})
+
+        self.varspresets = {"wd_vars" : wdlang.wd_vars, "presets" : wdlang.presets}
+
+    return self.Organize(ret)
+
+  def Transform_to_tkname(self, widg) -> str:
+    widg = widg.replace(".wd", "").replace("__init__", "")
+
+    ret :str = widg[len(str(self.Src_folder)):].replace("/", ".").replace("\\", ".")
+
+    if ret[-1] == ".":
+      return ret[:max(1, len(ret)-1)]
+    
+    return ret
+  
+  def Chek_name(self, master : str, widget : str) -> str:
+    ret : list = [*master.split(sep = ".")]
+    
+    widget = widget.replace(master, "" , 1)
+
+    for i in widget.split(sep = "."):
+      if not i in ret[-1]:
+        ret.append(i)
+      
+    return ".".join(ret).replace("..", ".")
+    
+  def Organize(self, widgets : dict) -> dict:
+    
+    ret     : dict  = {}
+    sep_len : dict  = {} 
+
+    for i in [a.split(sep = ".") for a in widgets.keys()]:
+      ln = len([ a for a in i if a != ""])
+
+      if not ln in sep_len:
+        sep_len[ln] = []
+
+      sep_len[ln].append(i)
+
+
+    inl : list      = list(sep_len.keys())
+    inl.sort()
+
+    for i in inl:
+      for a in sep_len[i]:
+        name = ".".join(a)
+        ret[name] = widgets[name]
+
+    return ret
+
+if __name__ == "__main__":
+  Files_structure("Pages")
---
+++ wdlang/tkwd.py
@@ -0,0 +1,90 @@
+from .Files import Files_structure
+import tkinter
+import tkinter.ttk
+
+class Constuct:
+  def __init__(self, Src_app_name : str, Src_app_method , Src_Pages : str, Src_functions : str) -> None:
+    self.masters      : dict  = {Src_app_name : Src_app_method}
+    self.scripts_call : dict  = {} 
+
+    self.Create_widget(Src_app_method, Files_structure(Src_Pages, Src_functions))
+
+  def Create_widget(self, Src_method, widgets : dict) -> None:
+    scripts      : dict = {}
+
+    for tk_name, values in widgets.items():
+      try:
+        widg = Src_method.nametowidget(tk_name)
+
+      except:
+        master_name = tk_name[: max(1, tk_name.rfind("."))]
+
+        if master_name in self.masters:
+          master = self.masters[master_name]
+        else:
+          raise ValueError(master_name, self.masters)
+        
+        tk_type = values['self']['tk_type']
+        name    : str = tk_name.replace(master_name+".", "").replace(master_name, "")
+        module_call   = tkinter
+
+        if "ttk." in tk_type:
+          module_call = tkinter.ttk
+          tk_type = tk_type.replace("ttk.", "")
+        
+        widg = getattr(module_call, tk_type)(master, name = name)
+        self.masters[tk_name] = widg
+
+      atrbs : dict  = {}
+      cmds  : dict  = {}
+
+      if "presets" in values["self"]:
+        prest_names = [values["self"]["presets"]]
+        
+        if type(prest_names) == str:
+          prest_names = [prest_names]
+
+        for a in prest_names:
+          preset_val = widgets.varspresets["presets"][a]["self"]
+
+          if "bg" in preset_val:
+            preset_val["background"] = preset_val["bg"]
+            preset_val.pop("bg")
+
+          if "fg" in preset_val:
+            preset_val["foreground"] = preset_val["fg"]
+
+            preset_val.pop("fg")
+          
+          atrbs.update(preset_val)
+
+      for a in values["segments"]["atrb"]:
+        atrbs.update(a)
+
+      if widg.__module__ == "tkinter.ttk":
+        pass
+      else:
+        widg.configure(**atrbs)
+
+      for a in values["segments"]["cmds"]:
+        cmds.update(a)
+      
+      for cmd_name, ka in cmds.items():
+        args = ka['args']
+
+        if type(args) != list:
+          args = [args]
+          
+        exec(f"widg.{cmd_name}(*args, **ka['kwargs'])")
+
+      if values["script"]:    
+        scripts[values["script"]] = widg
+    
+    self.Run_scripts(scripts)
+
+  def Run_scripts(self, scripts):
+    for k, v in scripts.items():
+      self.scripts_call[v.winfo_name()] = k(v, self)
+  
+  def get_scripts(self, name):
+    return self.scripts_call.get(name)
